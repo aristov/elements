9a86c744da7841f990926a28b5d9d5bea5885b5f
aristov <vv.aristov@gmail.com>
2014-03-25 20:02:56 +0400

    edit enb-make: remove production mode, add bem-core levels, use `browser.js` instead `js` tech

--- .bem/enb-make.js
+++ .bem/enb-make.js
@@ -1,21 +1,4 @@
 module.exports = function(config) {
-    config.mode('development', function() {
-        config.node('bundles/index', function(nodeConfig) {
-            nodeConfig.addTechs([
-                [ require('enb/techs/file-copy'), { sourceTarget: '?.js', destTarget: '_?.js' } ],
-                [ require('enb/techs/file-copy'), { sourceTarget: '?.css', destTarget: '_?.css' } ]
-            ]);
-        });
-    });
-    config.mode('production', function() {
-        config.node('bundles/index', function(nodeConfig) {
-            nodeConfig.addTechs([
-                [ require('enb/techs/borschik'), { sourceTarget: '?.js', destTarget: '_?.js', minify: true, freeze: false } ],
-                [ require('enb/techs/borschik'), { sourceTarget: '?.css', destTarget: '_?.css', minify: true, freeze: false } ]
-            ]);
-        });
-    });
-
     config.node('bundles/index', function(nodeConfig) {
         nodeConfig.addTechs([
             [ require('enb/techs/levels'), { levels: getLevels() } ],
@@ -25,13 +8,18 @@ module.exports = function(config) {
             require('enb/techs/files'),
             require('./enb-techs/bemjst'),
             require('./enb-techs/html-from-bemjson'),
-            require('enb/techs/js'),
-            require('enb/techs/css')
+            [ require('enb/techs/browser-js'), { target: '?.js' } ],
+            require('enb/techs/css'),
+
+            [ require('enb/techs/file-copy'), { sourceTarget: '?.js', destTarget: '_?.js' } ],
+            [ require('enb/techs/file-copy'), { sourceTarget: '?.css', destTarget: '_?.css' } ]
         ]);
         nodeConfig.addTargets(['?.html', '_?.js', '_?.css']);
 
         function getLevels() {
             return [
+                { path: 'libs/bem-core/common.blocks', check: false },
+                { path: 'libs/bem-core/desktop.blocks', check: false },
                 { path: 'blocks', check: true }
             ].map(function(l) { return config.resolvePath(l); });
         }
